70e142ad051073c303fb843945142ce2596294a1
Ugbedeojo Abba <140442305+Ugbe@users.noreply.github.com>
2025-07-25 16:27:18 +0100

    first upload

---
+++ src/models.py
@@ -0,0 +1,11 @@
+from datetime import date
+
+class Task:
+    def __init__(self, id:int, description:str, priority:str, due_date:date=None, status:str="Pending", archived:bool=False):
+        self.id = id
+        self.description = description
+        self.due_date = due_date
+        self.status = status
+        self.archived = archived
+        self.priority = priority
+    
